6c528b32f3a3777628ec80f84021a84e7bdff615
davidabsillis <davidabsillis@hotmail.be>
2019-04-14 15:36:33 +0200

    EfectClass => Utils

--- src/core/EffectClass.cpp
+++
@@ -1,4 +0,0 @@
-
-#include "EffectClass.h"
-
-	
---
+++ src/core/Utils.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+
+class Utils {
+public:
+	enum effect {
+		PHYSICAL,
+		MAGIC,
+		POISON,
+		BURN,
+		SLOW,
+		STUN,
+		ONESHOT,
+		BLEED,
+		HEAL,
+		SPEEDUP,
+		REVIVE,
+		INVINCIBLE,
+		INVISIBLE,
+	};
+};
